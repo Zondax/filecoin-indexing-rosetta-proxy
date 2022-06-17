67a048933d1334186203e334a2302cef59cf3890
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2022-06-17 12:00:18 -0300

    Add /call endpoint Add VersionedStateCompute to call endpoint

---
+++ services/call/call.go
@@ -0,0 +1,39 @@
+package call
+
+import (
+	"context"
+	"github.com/coinbase/rosetta-sdk-go/server"
+	rosettaTypes "github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/filecoin-project/lotus/api"
+	"github.com/zondax/filecoin-indexing-rosetta-proxy/tools/parser"
+	rosetta "github.com/zondax/rosetta-filecoin-proxy/rosetta/services"
+)
+
+type CallAPIService struct {
+	network        *rosettaTypes.NetworkIdentifier
+	node           api.FullNode
+	traceRetriever *parser.TraceRetriever
+}
+
+// NewCallAPIService creates a new instance of a CallAPIService.
+func NewCallAPIService(network *rosettaTypes.NetworkIdentifier, api *api.FullNode, retriever *parser.TraceRetriever) server.CallAPIServicer {
+	return &CallAPIService{
+		network:        network,
+		node:           *api,
+		traceRetriever: retriever,
+	}
+}
+
+func (s *CallAPIService) Call(ctx context.Context, request *rosettaTypes.CallRequest) (*rosettaTypes.CallResponse, *rosettaTypes.Error) {
+	errNet := rosetta.ValidateNetworkId(ctx, &s.node, request.NetworkIdentifier)
+	if errNet != nil {
+		return nil, errNet
+	}
+
+	switch request.Method {
+	case StateComputeCall:
+		return s.StateComputeVersioned(ctx, request)
+	default:
+		return nil, rosetta.BuildError(rosetta.ErrOperationNotSupported, nil, true)
+	}
+}
